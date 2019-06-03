7ef2e432546ec64662b3a21bb30b09b521a9ac63
migueldsg <migueldsg0904@gmail.com>
2019-06-03 17:31:27 +0200

    Ajout d'un paragraphe à l'index

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <h1>Teste</h1>
     <div>
         <p>Ajout d'un texte depuis une autre branche teste</p>
+
+        <p>
+        	Denique Antiochensis ordinis vertices sub uno elogio iussit occidi ideo efferatus, quod ei celebrari vilitatem intempestivam urgenti, cum inpenderet inopia, gravius rationabili responderunt; et perissent ad unum ni comes orientis tunc Honoratus fixa constantia restitisset.
+        </p>
     </div>
 </body>
 </html>
