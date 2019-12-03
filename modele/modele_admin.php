feae9fe25eb4861f0ad2d748fdbbf03d13846c0f
Lokman <lokman-hekim@hotmail.fr>
2019-12-03 12:00:46 +0100

    refacto bdd + code

--- modele/modele_admin.php
+++ modele/modele_admin.php
@@ -1,13 +1,15 @@
 <?php
-
-include '../includes/identifiants_bdd.php';
-
 class ModeleAdmin
 {
 	private $unPdo ;
 
-	public function __construct ()
-	{/	}
+	public function __construct ($serveur, $bdd, $user, $mdp)
+	{
+		$this->unPdo = null;
+		try{
+		//connexion à la base de données en utlisant la classe PDO
+		$this->unPdo = new PDO ("mysql:host=".$serveur.";dbname=".$bdd,$user,$mdp);
+		}
 		catch (PDOException $exp)
 		{
 			echo "Erreur de connexion à la base de données";
